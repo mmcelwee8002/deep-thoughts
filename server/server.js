b07edc37d19f0af6d780f63f29a03065ae5f476e
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-05-02 13:44:50 -0500

    lesson 2 completed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -10,7 +12,8 @@ const app = express();
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: authMiddleware,
   });
   await server.start();
   server.applyMiddleware({ app });
